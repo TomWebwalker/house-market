b2766d9f8320fe22d12807898d4e1fa14a5d1988
Tomasz Flis <tomaszwwl@gmail.com>
2024-10-04 21:42:58 +0200

    include taiga ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import {TuiButton, TuiRoot, TuiTitle} from "@taiga-ui/core";
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {TuiProgress} from '@taiga-ui/kit';
+import {TuiAppBar} from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, TuiRoot, TuiAppBar, TuiButton, TuiProgress, TuiTitle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
